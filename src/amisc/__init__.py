9530fef8d8f6ef64b2d50fb376d2bf92601de059
Joshua Eckels <eckelsjd@umich.edu>
2023-12-05 19:29:07 -0500

    Initialize pdm project

---
+++ src/amisc/__init__.py
@@ -0,0 +1,6 @@
+"""Adaptive multi-index stochastic collocation (surrogates) for metamodeling of multidisciplinary systems.
+
+- Author - Joshua Eckels (eckelsjd@umich.edu)
+- License - GNU GPLv3
+"""
+__version__ = "0.0.0"
