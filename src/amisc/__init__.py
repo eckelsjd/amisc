06c089a82438aaee1dd001c2eb7c9f146917173e
Joshua Eckels <eckelsjd@umich.edu>
2023-12-13 09:53:06 -0500

    updating version

--- src/amisc/__init__.py
+++ src/amisc/__init__.py
@@ -8,7 +8,7 @@ import numpy as np
 from amisc.interpolator import BaseInterpolator
 from amisc.rv import BaseRV
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # Custom types that are used frequently
 IndexSet = list[tuple[tuple, tuple]]
